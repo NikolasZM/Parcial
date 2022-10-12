a587bc298fffbb5e9b516f50f58ff0a892349139
Jean-Marc-NH <101827583+Jean-Marc-NH@users.noreply.github.com>
2022-10-12 16:04:16 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -8,6 +8,10 @@
 
 using namespace std;
 
+#define	GREEN 	"\x1B[32m"
+#define	OTRO 	"\x1B[30m"
+#define	DF 		"\x1B[39m"
+
 
 int main() {
 	
@@ -19,10 +23,8 @@ int main() {
 	while (true) {
 		switch(opt) {
 			case 0:
-				cout<<"\nElija una opcion:\n<1> Crea una matriz con valores aleatorios.\n<2>Imprima la matriz\n<3>Modifica un valor especifico de la matriz.\n
-				<4>Convierte los valores en la diagonal de la matriz por valores igual a cero.\n<5>Ingresa 3 valores y se buscara cuantas veces se encuentran esos
-				valores en la matriz\n<8>Transpuesta";
-				cout<<"\n>>";
+				cout<<"\nElija una opcion:\n<1> Crea una matriz con valores aleatorios.\n<2>Imprima la matriz\n<3>Modifica un valor especifico de la matriz.\n<4>Convierte los valores en la diagonal de la matriz por valores igual a cero.\n<5>Ingresa 3 valores y se buscara cuantas veces se encuentran esos valores en la matriz\n<7>Multiplos de 5 por 10.\n<8>Transpuesta";
+				cout<<"\n" << ">>";
 				cin>>opt;
 				break;
 			case 1:
@@ -51,11 +53,15 @@ int main() {
                 cout<<"\nIngresa el tercer numero a buscar\n>>";
                 cin>>c;
                 funcion_05(matriz,a,b,c,a1,b1,c1);
-                cout<<"El numero "<<a<<" se repite "<<a1<<" veces.\n";
-                cout<<"El numero "<<b<<" se repite "<<b1<<" veces.\n";
-                cout<<"El numero "<<c<<" se repite "<<c1<<" veces.\n";
+                cout<< GREEN <<"El numero "<<a<<" se repite "<<a1<<" veces.\n" << DF;
+                cout<< GREEN <<"El numero "<<b<<" se repite "<<b1<<" veces.\n" << DF;
+                cout<< GREEN <<"El numero "<<c<<" se repite "<<c1<<" veces.\n" << DF;
                 opt=0;
                 break; }	
+			case 7:
+				funcion_07(matriz);
+				opt=0;
+				break;
 			case 8:
 				funcion_08(matriz);
 				opt=0;
