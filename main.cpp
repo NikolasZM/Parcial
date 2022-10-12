6610f1f2a88ddfd8a3c4a402e120063fa6dc5b89
Jean-Marc-NH <101827583+Jean-Marc-NH@users.noreply.github.com>
2022-10-12 15:11:02 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,52 +4,17 @@
 #include <stdlib.h>
 #include <time.h>
 #include <iomanip>
+#include "libreria.h"
+
 using namespace std;
+
 #define	GREEN 	"\x1B[32m"
 #define	DF 		"\x1B[39m"
 
-void crearMatriz(int (&mat)[10][10]) {		//funcion_01
-	srand(time(NULL));
-	for (int i{0};i<10;++i) {
-		for (int j{0};j<10;++j) {
-			mat[i][j]=rand() % 100;
-		}
-	}
-	cout<<GREEN<<"\nMatriz Creada\n"<<DF;
-}
-
-void mostrarMatriz(int mat[10][10]) {		//funcion_02
-	for (int i{0};i<10;++i) {
-		cout<<"| ";
-		for (int j{0};j<10;++j) {	
-			cout<<setw(3)<<mat[i][j]<<" ";
-		}
-		cout<<"|\n";
-	}
-}
-
-void funcion_03(int (&mat)[10][10]) {
-	int fil,col,x;
-	cout<<"\nIngresa el valor de las filas.";
-	cin>>fil;
-	cout<<"\nIngresa el valor de las columnas.";
-	cin>>col;
-	cout<<"\nIngresa el nuevo valor.";
-	cin>>x;
-	mat[fil][col]=x;
-	cout<<GREEN<<"\nEl valor fue modificado exitosamente.\n"<<DF;
-
-}
 
 int main() {
-	cout << "   _______________________________________________\n";
-	cout << " /|                                               |\n";
-	cout << "| |           Jean Marc Nadeau Huamani            |\n";
-	cout << "| |         Nikolas Ymanol Zuñiga Monroy          |\n";
-	cout << "| |                     CComp1                    |\n";
-	cout << "| |_______________________________________________|\n";
-	cout << "|/________________________________________________/\n";
-	cout << "--------------------Examen_Parcial-----------------\n\n";
+	
+    encabezado("Examen_parcial");
 	
 	std::system("cls");
 	int opt{0};
